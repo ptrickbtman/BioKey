3d06567eb563f7969393dd16290cde552eaf7e40
ptrickbtman <57462954+ptrickbtman@users.noreply.github.com>
2020-08-24 23:26:35 -0400

    Hamburger to Menu

---
+++ webSite/js/cargado.js
@@ -0,0 +1,17 @@
+$(window).on("load", function() {
+    deleteLoader();
+})
+
+function deleteLoader() {
+    $(".loader").css("opacity", "0");
+    setTimeout(function() {
+        $(".loader").remove();
+    }, 500)
+}
+
+
+$(".hamburger").on("click", function() {
+    $(".stick1").toggleClass("stick1On");
+    $(".stick2").toggleClass("stick2On");
+    $(".stick3").toggleClass("stick3On")
+});
