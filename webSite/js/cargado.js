3c578cc3773281d577cc816ef6c5e29f50632461
ptrickbtman <57462954+ptrickbtman@users.noreply.github.com>
2020-08-27 15:13:44 -0400

    test1
    Prototipo login - index - menu

--- webSite/js/cargado.js
+++ webSite/js/cargado.js
@@ -1,12 +1,14 @@
 $(window).on("load", function() {
-    deleteLoader();
+    setTimeout(deleteLoader(), 500);
 })
 
 function deleteLoader() {
+
     $(".loader").css("opacity", "0");
     setTimeout(function() {
         $(".loader").remove();
     }, 500)
+
 }
 
 
@@ -15,4 +17,44 @@ $(".hamburger").on("click", function() {
     $(".stick2").toggleClass("stick2On");
     $(".stick3").toggleClass("stick3On")
     $(".itemsMenu").toggleClass("itemsMenuOn")
+});
+
+
+// login modal
+
+$(".abrirModal").on("click", function() {
+
+    $(".contLogin").addClass("contLoginOn")
+    $(".modalLogin").addClass("modalLoginOn");
+    setTimeout(function() {
+        $(".contLoginBinary").addClass("contLoginBinaryOn");
+        $(".contModalLogin").addClass("contModalLoginOn");
+        setTimeout(function() {
+            $(".btnEnvio").css("opacity", "0");
+            setTimeout(function() {
+                $(".btnEnvio").css("opacity", "1");
+                setTimeout(function() {
+                    $(".btnEnvio").css("opacity", "0");
+                    setTimeout(function() {
+                        $(".btnEnvio").css("opacity", "1");
+                    }, 100)
+                }, 200)
+            }, 200)
+
+
+        }, 350)
+    }, 600)
+});
+
+
+
+$(".cerrarLogin").on("click", function() {
+    $(".contLoginBinary").removeClass("contLoginBinaryOn");
+    $(".contModalLogin").removeClass("contModalLoginOn");
+
+    setTimeout(function() {
+        $(".contLogin").removeClass("contLoginOn")
+        $(".modalLogin").removeClass("modalLoginOn");
+
+    }, 700)
 });
